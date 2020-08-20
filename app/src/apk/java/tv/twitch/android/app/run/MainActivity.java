66e2f1335a5566d003877155796a3e969d615154
Roman <nopbreak@gmail.com>
2020-08-20 13:15:37 +0300

    apk: fix build

--- app/src/apk/java/tv/twitch/android/app/run/MainActivity.java
+++ app/src/apk/java/tv/twitch/android/app/run/MainActivity.java
@@ -1,11 +1,12 @@
 package tv.twitch.android.app.run;
 
 
-import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
